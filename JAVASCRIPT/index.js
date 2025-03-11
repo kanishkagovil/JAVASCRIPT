89c62ea0ef2b00ca7383b6c71f154ed5a4405551
Kanishka govil <149759260+kanishkagovil@users.noreply.github.com>
2025-03-11 13:52:01 +0530

    Add files via upload

---
+++ JAVASCRIPT/index.js
@@ -0,0 +1,40 @@
+// document.write("hello jai bajrangbali");
+console.log("kanishka")
+
+// var firstname="kanishka govil";
+// firstname="kannu";
+// document.write(firstname);
+
+// var x=3;
+// var y=8;
+// var z=9;
+// var a=z*y*x;
+// document.write(a);
+// const price1 = 5;
+// const price2 = 6;
+// let total = price1 + price2;
+// console.log(total);
+
+
+
+
+
+
+// var ram=30;
+// var krishna=40;
+// var krishna=90;
+// var shyam= ram * krishna;
+// document.write(shyam
+// );
+// let ram=30;
+// let krishna=40;
+// // let krishna=90;
+// let shyam= ram * krishna;
+// document.write(shyam
+// );
+const ram=30;
+const krishna=40;
+// const krishna=90;
+const shyam= ram * krishna;
+document.write(shyam
+);
